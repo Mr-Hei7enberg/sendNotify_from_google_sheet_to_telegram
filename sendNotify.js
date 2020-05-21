d90b6d4b205d2399287af3b2a1531c307ee3027b
Mr-Hei7enberg <63899989+Mr-Hei7enberg@users.noreply.github.com>
2020-05-21 16:17:39 +0300

    v1.0
    send notifications of changes in telegrams from google tables

---
+++ sendNotify.js
@@ -0,0 +1,59 @@
+const ss = SpreadsheetApp.openById('1Bgn5G7Xj_m6ZiNdsKqOQ4hjkCWzVaHlLVtpdjQuP4xs');
+const sheet = ss.getSheetByName('Log');
+
+
+function sendStat(){
+  
+  let newValues = sheet.getRange(2, 5, sheet.getLastRow()-1).getValues().map(item => item[0]);
+  let oldValues = sheet.getRange(2, 5, sheet.getLastRow()-1).getNotes().map(item => item[0]);
+   
+  for (let i = 0; i < newValues.length; i++){ // перебираем циклом весь массив
+    let name = sheet.getRange(i+2, 4).getValue(); // достаем имя из массива
+    
+    if (newValues[i] != '') { // если массив не пустой
+      if (oldValues[i] == ''){ // если массив старых значений пустой присваиваем баланс = 0
+        //let name = sheet.getRange(i+2, 4).getValue(); // достаем имя из массива
+        sheet.getRange(i+2, 5).setNote(newValues[i]); // присваиваем старые значения
+        sendMessage(187051021, `${name} balance changed from: 0 → to ${newValues[i]} grn`); // отправляем в телеграм
+      } else if (newValues[i] != oldValues[i]){ // если значения из старого и нового массива не совпадают
+        //let name = sheet.getRange(i+2, 4).getValue(); // достаем имя из массива
+        sheet.getRange(i+2, 5).setNote(newValues[i]); // присваиваем старые значения
+        sendMessage(187051021, `${name} balance changed from: ${oldValues[i]} → to ${newValues[i]} grn`); // отправляем в телеграм
+        } 
+    } 
+    
+  } // end for
+  
+} //end sendStat
+
+
+
+function setTriggerTime() {
+  ScriptApp.newTrigger("sendStat")
+  .timeBased()
+  .everyMinutes(1)
+  .create();
+}
+
+
+function deleteTrigger(){
+  // Deletes all triggers in the current project.
+  let triggers = ScriptApp.getProjectTriggers();
+  for (let i = 0; i < triggers.length; i++) {
+    ScriptApp.deleteTrigger(triggers[i]);
+  }
+}
+
+function sendMessage(id_chat, text, keyboard) { // Отправляет сообщение используя sendMessage
+  var data = {
+    method: "post",
+    payload: {
+      method: "sendMessage",
+      chat_id: String(id_chat),
+      text: text,
+      parse_mode: "HTML",
+      reply_markup: JSON.stringify(keyboard)
+    }
+  };
+  UrlFetchApp.fetch('https://api.telegram.org/bot' + token + '/', data);
+}
